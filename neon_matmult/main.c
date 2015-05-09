93c626654f22fea473b085e48b0a875b77b62904
sauravwcm <s.paul@student.tudelft.nl>
2015-05-09 15:13:18 +0200

    final neon multiplication

--- neon_matmult/main.c
+++ neon_matmult/main.c
@@ -2,119 +2,158 @@
 #include <stdlib.h>
 #include "arm_neon.h"
 
-#define ROW1 32
-#define COL1 32
-#define ROW2 32
-#define COL2 32
+#define ROW1 64
+#define COL1 128
+#define ROW2 128
+#define COL2 64
 
-/*void add3 (uint8x16x2_t *data1, uint8x16x2_t *data2)
-{
-	int i;
-	static uint8_t a[COL2], b[COL2];
+void Timer_Init(void);
+void Timer_DeInit(void);
+long HiResTime(void);
 
-	vst2q_u8 (a, *data1);
-	vst2q_u8 (b, *data2);
-    	data2 = vaddq_u8 (*data1, *data2);
-}
-*/
-void print_uint8 (uint8x16x2_t data, char* name)
-{
-    int i;
-    static uint8_t p[COL2];
-
-    vst2q_u8 (p, data);
-
-    printf ("%s = ", name);
-    for (i = 0; i < COL2; i++)
-    {
-        printf ("%02d ", p[i]);
-    }
-    printf ("\n");
-}
 
 int main ()
-{	int r=0, c=0, i=0;
-	int k=0, l=0, m=0;
-
-    /* Create custom arbitrary data. */
-   	uint8_t row1[COL2];
-	uint8_t row2[COL2];
-    	for(i=0; i<COL2; i++)
-	{	
-		row1[i] = 0;
-		row2[i] = 0;
-	}
-	uint8_t mat1[ROW1][COL1];
-	uint8_t mat2[ROW2][COL2];
+{	int r, c, i, j;
+	uint16_t mat1[ROW1][COL1], mat2[ROW2][COL2];
+	uint16_t res[ROW1][COL2];
+	uint16x8_t vec1, vec2;
+	uint16_t row[8], col[8];
+	uint8_t count=0, step=0, rem=0, que=0;
 	
+	long tStart, tEnd;
+	Timer_Init();
+
 	for(r=0; r<ROW1; r++)
 		for(c=0; c<COL1; c++)
-		{
-			mat1[r][c] = 1;
-		}
+			mat1[r][c] = 2;
+
 	for(r=0; r<ROW2; r++)
 		for(c=0; c<COL2; c++)
-		{
 			mat2[r][c] = 1;
-		}
-
-	int res[ROW1*ROW2][COL2];
-	int Mat_Res[ROW1][COL2];
-
-	for(r=0; r<ROW1; r++)
-		{
-			for(c=0; c<COL1; c++)
-		     	{
-				for(l=0; l<COL2; l++)
-			 	{
-					res[k][l] = mat1[r][c]	* mat2[c][l];
-				}
-				k++;
-			}
-		}
+	if(ROW2%8==0)
+		step = ROW2/8;
+	else
+		step = (ROW2/8)+1;
 	
-/*	for(r=0; r<ROW1*ROW2;r++)
-	{
-		for(c=0;c<COL2;c++)
-		{
-			printf("%d ", res[r][c]);
-    		}
-		 printf("\n");
-	}
-*/	
-        printf("\n\n\n\n\n");
- 	uint8x16x2_t data1, data2;
-	
-	for(i=0; i<ROW1; i++)
-	{	data2 = vld2q_u8 (row2);
-		//print_uint8 (data2, "data2 \n");	
-		for(r=0; r<ROW2; r++)
-		{	
-			
-			for(l=0; l<COL2; l++)
+	que = ROW2/8;
+	rem = ROW2%8;
+
+	tStart = HiResTime();
+	for(i=0; i<ROW1;i++)
+	{	for(j=0; j<COL2; j++)
+		{	res[i][j] = 0;
+			while(count<step)
 			{
-				row1[l] = res[r+(ROW2*i)][l];
+				//for(r=0; r<8; r++)
+				//{
+				//	row[r] = 0;
+				//	col[r] = 0;
+				//}
+				vec1 = vmovq_n_u16 (0);
+				//vec1.val[0] = vmovq_n_u8 (0);
+				//vec1.val[1] = vmovq_n_u8 (0);
+				
+				vec2 = vmovq_n_u16 (0);
+				//vec2.val[0] = vmovq_n_u8 (0);
+				//vec2.val[1] = vmovq_n_u8 (0);
+				if(step-count == 1)
+				{	for(r=0; r<rem; r++)
+					{	
+						row[r]=mat1[i][r+count*8];
+						col[r]=mat2[r+count*8][j];
+						row[8-rem] = 0;
+						col[8-rem] = 0;
+					}
+				}
+				else
+				{
+					for(r=0; r<8; r++)
+					{	
+						row[r]=mat1[i][r+count*8];
+						col[r]=mat2[r+count*8][j];
+					}
+				}
+				vec1 = vld1q_u16 (row);
+				vec2 = vld1q_u16 (col);
+				vec1 = vmulq_u16(vec1, vec2);
+				//vec1.val[0] = vmulq_u8(vec1.val[0], vec2.val[0]);
+				//vec1.val[1] = vmulq_u8(vec1.val[1], vec2.val[1]);
+			
+				vst1q_u16 (row, vec1);
+
+				for(r=0; r<8; r++)
+				{
+					res[i][j]+= row[r];
+				}
+				count++;
 			}
-			data1 = vld2q_u8 (row1);
-			//print_uint8 (data1, "data1 \n");
-			//add3(&data1, &data2);
-			data2.val[0] = vaddq_u8 (data1.val[0], data2.val[0]);
-			data2.val[1] = vaddq_u8 (data1.val[1], data2.val[1]);
-		}
-		vst2q_u8 (row1, data2);
-		for(c=0; c<COL2; c++)
-		{
-			Mat_Res[i][c] = row1[c];
+			count=0;
 		}
 	}
-	printf("Resultant Matrix \n \n");
-	for(r=0; r<ROW1; r++)
+	tEnd = HiResTime();
+	printf("result matrix \n");
+
+	for(i=0;i<ROW1;i++)
 	{
-		for(c=0; c<COL2; c++)
-		{
-			printf("%d ", Mat_Res[r][c]);
-		}
+		for(j=0;j<COL2;j++)
+		printf("%d ", res[i][j]);
+
 		printf("\n");
-	}
+        }
+	printf("\n Execution Time = %d \n", (tEnd - tStart) );
+    	return 0;
+}
+
+
+//timer functions
+
+void Timer_Init(void)
+{
+#if defined(__arm__)
+    int user_enable;
+#if defined(__GNUC__)
+#define COMPILER_ID "GCC"
+asm ("mrc p15, 0, %0, c9, c14, 0" : "=r"(user_enable));
+#elif defined(__CC_ARM)
+#define COMPILER_ID "ARMCC"
+#error "ARMCC not implemented"
+#else
+#error  "Unknown ARM compiler"
+#endif
+    if (!user_enable)
+        printf("User mode enable is not set, no access to cycle counter\n");
+
+#if defined(__GNUC__)
+    /* Reset and enable */
+    asm volatile ("mcr p15, 0, %0, c9, c12, 0" :: "r"(5));
+    /* Enable cycle counter */
+    asm volatile ("mcr p15, 0, %0, c9, c12, 1" :: "r"(0x80000000));
+    /* Reset overflow flags */
+    asm volatile ("mcr p15, 0, %0, c9, c12, 3" :: "r"(0x80000000));
+#endif
+
+#else
+    printf("No support for RDTSC on this CPU platform\n");
+#endif /* defined(__arm__) */
+}
+
+void Timer_DeInit(void)
+{
+#if defined(__GNUC__)
+    /* Disable */
+    asm volatile ("mcr p15, 0, %0, c9, c12, 0" :: "r"(0));
+#endif
+}
+
+long HiResTime(void)
+{
+    long cycles;
+#if defined(__arm__)
+#if defined(__GNUC__)
+asm volatile ("mrc p15, 0, %0, c9, c13, 0": "=r"(cycles));
+#endif
+    return cycles;
+#else
     return 0;
+#endif
 }
